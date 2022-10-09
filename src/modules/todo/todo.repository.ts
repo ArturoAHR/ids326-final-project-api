a9d19570349d8ec1d5f10cc65d9610ce30a057f8
RukShu <arturoaureliohr@gmail.com>
2022-10-09 18:26:54 -0400

    Add getAll and getById methods in Todo repository

--- src/modules/todo/todo.repository.ts
+++ src/modules/todo/todo.repository.ts
@@ -2,4 +2,12 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Todo } from '../../entities/todo.entity';
 
 @EntityRepository(Todo)
-export class TodoRepository extends Repository<Todo> {}
+export class TodoRepository extends Repository<Todo> {
+  async getAll(): Promise<Todo[]> {
+    return await this.createQueryBuilder('todo').getMany();
+  }
+
+  async getById(id: string): Promise<Todo> {
+    return await this.findOneOrFail({ id: id });
+  }
+}
