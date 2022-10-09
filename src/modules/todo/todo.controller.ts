b4adc236db01af5d9818880c99ba43da5e8ca12a
RukShu <arturoaureliohr@gmail.com>
2022-10-09 18:37:18 -0400

    Add basic CRUD endpoints

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateTodoRequestDTO } from '../../dto/todo/create-todo-request.dto';
+import { UpdateTodoRequestDTO } from '../../dto/todo/update-todo-request.dto';
+import { Todo } from '../../entities/todo.entity';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getAll(): Promise<Todo[]> {
+    return await this.todoService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<Todo> {
+    return await this.todoService.getById(id);
+  }
+
+  @Post()
+  async create(@Body() dto: CreateTodoRequestDTO): Promise<Todo> {
+    return await this.todoService.create(dto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateTodoRequestDTO,
+  ): Promise<Todo> {
+    return await this.todoService.update(id, dto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<boolean> {
+    return await this.todoService.delete(id);
+  }
+}
