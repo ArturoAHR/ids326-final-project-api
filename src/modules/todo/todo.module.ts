975d269b1314a1cda12c6b82d4d00e38c6c12db5
RukShu <arturoaureliohr@gmail.com>
2022-10-09 18:24:41 -0400

    Import Todo repository in Todo module

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from '../../entities/todo.entity';
 import { TodoController } from './todo.controller';
+import { TodoRepository } from './todo.repository';
 import { TodoService } from './todo.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todo, TodoRepository])],
   controllers: [TodoController],
   providers: [TodoService],
 })
